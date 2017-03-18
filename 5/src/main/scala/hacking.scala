876c16a9f70ae32b4eab6fba975c11bccc7f9076
Ben Yelsey <byelsey1@gmail.com>
2017-03-18 16:49:03 -0400

    refactoring

--- 5/src/main/scala/hacking.scala
+++ 5/src/main/scala/hacking.scala
@@ -2,21 +2,22 @@ object Hacking extends App {
   val input = "ojvtpuvg"
   val messageDigest = java.security.MessageDigest.getInstance("md5")
 
-
-  def solvePartB(id: String) = {
-
-  }
-
-  def solvePartA(id: String): String = {
-    Iterator.from(0, 1)
+  def fiveZeroesIterator(id: String): Iterator[String] = {
+    Iterator.from(0)
       .map {id + _}
       .map { string =>
         val bytes = messageDigest.digest(string.getBytes)
         new java.math.BigInteger(1, bytes).toString(16)
       }
       .filter {_.length == 27}
-      .take(8)
-      .foldLeft("") {_ + _(0)}
+  }
+
+  def solvePartB(id: String): String = {
+    ""
+  }
+
+  def solvePartA(id: String): String = {
+    fiveZeroesIterator(id).take(8).foldLeft("") {_ + _(0)}
   }
 
   try {
