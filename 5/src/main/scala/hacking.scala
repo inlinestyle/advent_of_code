2402720966d14ce5ff416d9580b58b926b8c5060
Ben Yelsey <byelsey1@gmail.com>
2017-03-18 18:10:43 -0400

    yayayy

--- 5/src/main/scala/hacking.scala
+++ 5/src/main/scala/hacking.scala
@@ -1,6 +1,9 @@
 object Hacking extends App {
   val input = "ojvtpuvg"
+
   val messageDigest = java.security.MessageDigest.getInstance("md5")
+
+  val emptyPassword: Seq[Option[Char]] = Seq(None, None, None, None, None, None, None, None)
 
   def fiveZeroesIterator(id: String): Iterator[String] = {
     Iterator.from(0)
@@ -9,11 +12,23 @@ object Hacking extends App {
         val bytes = messageDigest.digest(string.getBytes)
         new java.math.BigInteger(1, bytes).toString(16)
       }
-      .filter {_.length == 27}
+      .filter {_.length <= 27}
+      .map {"%27s".format(_).replace(' ', '0')}
   }
 
   def solvePartB(id: String): String = {
-    ""
+    fiveZeroesIterator(id)
+      .scanLeft(emptyPassword) { (password, hash) =>
+        val index = hash(0).asDigit
+        if (index < 8 && password(index) == None)
+          password.updated(index, Some(hash(1)))
+        else password
+      }
+      .dropWhile {password => !password.forall(_ != None)}
+      .take(1)
+      .flatten
+      .flatten
+      .mkString
   }
 
   def solvePartA(id: String): String = {
@@ -24,7 +39,7 @@ object Hacking extends App {
     println("Part A:")
     println(solvePartA(input))
     println("Part B:")
-    solvePartB(input)
+    println(solvePartB(input))
   } catch {
     case exception: Exception => println(exception)
   }
