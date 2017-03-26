61cc18126fedd30d48a91023da6750c1229d8c68
Ben Yelsey <byelsey1@gmail.com>
2017-03-26 16:29:25 -0400

    part b

--- 6/src/main/scala/signal.scala
+++ 6/src/main/scala/signal.scala
@@ -3,14 +3,23 @@ import scala.io.Source
 object Signal extends App {
   val filename = "input.txt"
 
+  def sortByFrequency(list: Seq[Char]): Seq[Char] = {
+    list.groupBy(identity)
+      .map(group => (group._1, group._2.length))
+      .toList
+      .sortBy(group => (-group._2, group._1))
+      .map(_._1)
+  }
+
+  def solvePartB(lines: Seq[String]): String = {
+    lines.transpose.map {
+      sortByFrequency(_).last
+    }.mkString
+  }
+
   def solvePartA(lines: Seq[String]): String = {
     lines.transpose.map {
-      _.groupBy(identity)
-        .map(group => (group._1, group._2.length))
-        .toList
-        .sortBy(group => (-group._2, group._1))
-        .map(_._1)
-        .head
+      sortByFrequency(_).head
     }.mkString
   }
 
@@ -18,6 +27,8 @@ object Signal extends App {
     val lines = Source.fromFile(filename).getLines.toList
     println("Part A:")
     println(solvePartA(lines))
+    println("Part B:")
+    println(solvePartB(lines))
   } catch {
     case exception: Exception => println(exception)
   }
