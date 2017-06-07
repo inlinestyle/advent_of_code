df7bf33cc4daf9168b1b0608b198a527278aac29
Ben Yelsey <byelsey1@gmail.com>
2017-06-07 13:10:45 -0400

    almost

--- 8/src/main/scala/screen.scala
+++ 8/src/main/scala/screen.scala
@@ -4,35 +4,62 @@ object Screen extends App {
   val filename = "input.txt"
 
   type Screen = List[List[String]]
-  val screen: Screen = List.fill(3)(List.fill(50)("."))
+  val numRows = 3
+  val numColumns = 50
+  val screen: Screen = List.fill(numRows)(List.fill(numColumns)("."))
 
   val rectPattern = "rect (\\d+)x(\\d+)".r
   val rotateColumnPattern = "rotate column x=(\\d+) by (\\d+)".r
   val rotateRowPattern = "rotate row y=(\\d+) by (\\d+)".r
 
-  def rect(screen: Screen, columns: String, rows: String): Screen = {
-    screen
+  def rect(screen: Screen, columns: Int, rows: Int): Screen = {
+    screen.zipWithIndex.map {
+      case (row, j) =>
+        if (j < columns) row.zipWithIndex.map {
+          case (column, i) =>
+            if (i < rows) "#"
+            else column
+        }
+        else row
+    }
   }
 
-  def rotateColumn(screen: Screen, x: String, by: String): Screen = {
-    screen
+  def rotateColumn(screen: Screen, x: Int, by: Int): Screen = {
+    screen.zipWithIndex.map {
+      case (row, j) =>
+        row.zipWithIndex.map {
+          case (column, i) =>
+            if (i == x) screen(Math.floorMod(j - by, numRows))(i)
+            else column
+        }
+    }
   }
 
-  def rotateRow(screen: Screen, y: String, by: String): Screen = {
-    screen
+  def rotateRow(screen: Screen, y: Int, by: Int): Screen = {
+    screen.zipWithIndex.map {
+      case (row, j) =>
+        if (j == y) row.zipWithIndex.map {
+          case (_, i) =>
+            screen(j)(Math.floorMod(i - by, numColumns))
+        }
+        else row
+    }
   }
 
   def solvePartA(lines: Seq[String]): String = {
     val resultScreen = lines.foldLeft(screen) {
       case (acc, rectPattern(columns, rows)) =>
-        rect(acc, columns, rows)
+        rect(acc, columns.toInt, rows.toInt)
       case (acc, rotateColumnPattern(x, by)) =>
-        rotateColumn(screen, x, by)
+        rotateColumn(acc, x.toInt, by.toInt)
       case (acc, rotateRowPattern(y, by)) =>
-        rotateRow(screen, y, by)
+        rotateRow(acc, y.toInt, by.toInt)
     }
-    println(resultScreen)
-    ""
+    resultScreen.foreach(row => println(row.mkString("")))
+    resultScreen.flatten.foldLeft(0) {
+      case (acc, pixel) =>
+        if (pixel == "#") acc + 1 else acc
+    }.toString
   }
 
   try {
